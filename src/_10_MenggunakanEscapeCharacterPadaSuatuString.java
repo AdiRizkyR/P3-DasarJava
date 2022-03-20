c84c35f216d7e268344e231526a29b42da1a93e5
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-20 23:26:18 +0700

    Program menggunakan escape character pada suatu string dengan java

---
+++ src/_10_MenggunakanEscapeCharacterPadaSuatuString.java
@@ -0,0 +1,10 @@
+public class _10_MenggunakanEscapeCharacterPadaSuatuString {
+    public static void main(String[] args) {
+        //Menggunakan escape character
+        String str1 = "POLBENG";
+        String str2 = "polbeng";
+        System.out.println(str1 + "\n" + str2);
+        String str3 = "RPL";
+        System.out.println(str1 + "\n" + str3);
+    }
+}
