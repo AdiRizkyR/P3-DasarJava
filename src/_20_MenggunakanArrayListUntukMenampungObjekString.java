a867bb81e860b45d98525eef074ef8800524cc1f
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-20 23:36:28 +0700

    Program menggunakan array list untuk menampung objek string dengan java

---
+++ src/_20_MenggunakanArrayListUntukMenampungObjekString.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class _20_MenggunakanArrayListUntukMenampungObjekString {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);//mencetak arraylist
+
+        //mengakses elemen arraylist menggunakan konvensional for
+        for (int i = 0; i < cars.size(); i++){
+            System.out.printf("%s ", cars.get(i));
+        }
+        System.out.println("");
+
+        //mengakases elemen arraylist menggunakan functional for each
+        cars.forEach((car) ->{
+            System.out.printf("%s ", car);
+        });
+        System.out.println();
+    }
+}
