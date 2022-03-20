90ced8d791aa0b44d01ad91c03cd22c0b6ced62b
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-20 23:24:28 +0700

    Program menangkap daftar argumen program dengan java

---
+++ src/_08_MenangkapDaftarArgumentProgram.java
@@ -0,0 +1,7 @@
+public class _08_MenangkapDaftarArgumentProgram {
+    public static void main(String[] args) {
+        System.out.println("Tanggal : " + args[0]);
+        System.out.println("Bulan : " + args[1]);
+        System.out.println("tahun : " + args[2]);
+    }
+}
