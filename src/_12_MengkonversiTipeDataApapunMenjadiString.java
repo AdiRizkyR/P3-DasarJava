997657d4d77df56f43725e852691c326facd35a3
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-20 23:28:36 +0700

    Program mengkonversi tipe data apapun menjadi string string dengan java

---
+++ src/_12_MengkonversiTipeDataApapunMenjadiString.java
@@ -0,0 +1,11 @@
+public class _12_MengkonversiTipeDataApapunMenjadiString {
+    public static void main(String[] args) {
+        //konversi tipe data apapun ke string
+        double nilai = 75;
+        nilai = nilai/2;
+        System.out.println("75/2 = " + nilai);
+        boolean isOK = true;
+        System.out.println("" + isOK);
+        System.out.println("" + 25/6.0);
+    }
+}
