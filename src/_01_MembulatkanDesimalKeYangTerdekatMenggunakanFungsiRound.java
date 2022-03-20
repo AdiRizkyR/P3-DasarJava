73aff13324071e8661aaa3d37323e307a119630c
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-20 18:55:42 +0700

    Program membulatkan angka ke desimal menggunakan fungsi round dengan java

--- src/_01_MembulatkanDesimalKeYangTerdekatMenggunakanFungsiRound.java
+++ src/_01_MembulatkanDesimalKeYangTerdekatMenggunakanFungsiRound.java
@@ -1,2 +1,13 @@
+import static java.lang.Math.round;
+
 public class _01_MembulatkanDesimalKeYangTerdekatMenggunakanFungsiRound {
+    public static void main(String[] args) {
+        double varDouble = 2.73;
+        long roundResult = round(varDouble);
+        System.out.printf("Pembulatan Round %.2f adalah %d\n", varDouble, roundResult);
+
+        varDouble = 2.49;
+        roundResult = round(varDouble);
+        System.out.printf("Pembuatan floor %.2f adalah %d\n", varDouble, roundResult);
+    }
 }
