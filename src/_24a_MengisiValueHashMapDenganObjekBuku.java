f3764230541a19ca4da5d5c1a2f74bf247f00de5
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-20 23:40:47 +0700

    Program mengisi value hash map dengan objek buku dengan java

---
+++ src/_24a_MengisiValueHashMapDenganObjekBuku.java
@@ -0,0 +1,26 @@
+public class _24a_MengisiValueHashMapDenganObjekBuku {
+
+    private String title;
+    private String author;
+
+    public _24a_MengisiValueHashMapDenganObjekBuku(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+}
